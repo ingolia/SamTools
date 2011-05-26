49967e303fd8209f86c3249779fce170a798dc22
nick <nick@ingolia.org>
2011-05-26 18:46:52 -0400

    Useful utility
    Ignore-this: 64695b70203821738fd9cce5babd78ef
    
    darcs-hash:20110526224652-fd04d-bec7e7c9d65f8f83393b91ce078a1228b0748754

---
+++ do-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+( cd samtools && \
+  runhaskell Setup.hs clean && \
+  runhaskell Setup.hs configure --user -p && \
+  runhaskell Setup.hs build && \
+  runhaskell Setup.hs haddock && \
+  runhaskell Setup.hs install ) && \
+( cd samtools-enumerator && \
+  runhaskell Setup.hs clean && \
+  runhaskell Setup.hs configure --user -p && \
+  runhaskell Setup.hs build && \
+  runhaskell Setup.hs haddock && \
+  runhaskell Setup.hs install ) && \
+( cd samtools-iteratee && \
+  runhaskell Setup.hs clean && \
+  runhaskell Setup.hs configure --user -p && \
+  runhaskell Setup.hs build && \
+  runhaskell Setup.hs haddock && \
+  runhaskell Setup.hs install )
